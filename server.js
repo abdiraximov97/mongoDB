ee0425a3b35f0340770393d8499f2c2620d8f8eb
abdiraximov97 <80697768+abdiraximov97@users.noreply.github.com>
2021-08-26 11:39:16 +0500

    initial server

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 
-const { urlencoded } = require("express");
 const express = require("express");
 const app = express();
+const path = require("path");
+const routes = require("./routes/routes");
 
 
 
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true,
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 
 app.set("view engine", "ejs");
 
-app.use(express.static(__dirname, "/public"));
+app.use(express.static(__dirname + "/public"));
 
-const port = process.env.port || 80;
+routes(app);
+
+const port = process.env.port || 8080;
 app.listen(port, () => {
     console.log(`server ${port} - portda yondi`);
 })
