46448dcc493fbe6fc2e86e49250d5363b008c0d3
abdiraximov97 <80697768+abdiraximov97@users.noreply.github.com>
2021-08-27 17:59:04 +0500

    front finish

--- routes/HomeRoute.js
+++ routes/HomeRoute.js
@@ -4,6 +4,10 @@ const router = require("express").Router();
 
 router.get("/", (req, res) => {
     res.render("index");
+});
+
+router.get("/signup", (req, res) => {
+    res.render("signup");
 })
 
 
